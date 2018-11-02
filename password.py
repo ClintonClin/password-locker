1c4607e44b60a8eb46d0ea4ef812301545dbb8ab
ClintonClin <clintonclin00@gmail.com>
2018-11-02 17:25:02 +0300

    created a class for the user

---
+++ password.py
@@ -0,0 +1,29 @@
+import random  
+from user import User
+from credentials import Credentials
+print("__________WELCOME TO PASSWORD-LOCKER__________")
+
+def createAccount(userName, passkey): 
+    
+    new_user = User(uname, pword)
+   
+    return new_user
+
+def login(login):
+
+
+if username != username and password != password:
+    print("Wrong credentials. Try again!!!")
+else:
+     print(" Select one option:\n 1. Store your credentials \n 2. Generate password ")
+
+choice = int(input())
+if choice == 1:
+        print("Enter account name e.g Facebook")
+        account = input()
+        print("Enter password to store of given account")
+        password = input()
+    
+
+
+
