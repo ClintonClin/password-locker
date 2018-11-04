ffe5f0d8e2eed8e258d9172f4192851b995a5ec1
ClintonClin <clintonclin00@gmail.com>
2018-11-05 01:03:00 +0300

    creating methods to test my class

--- user_test.py
+++ user_test.py
@@ -1,6 +1,7 @@
 import unittest
 import pyperclip
 from user import User, Info
+from password import register_user
 
 class TestUser (unittest.TestCase):
     '''
@@ -8,17 +9,26 @@ class TestUser (unittest.TestCase):
 	Args:
 	    unittest.TestCase: helps in creating test cases
 	'''
-    def setUp(self):
-		'''
-		Function to create a user account before each test
-		'''
-		self.new_user = User('Clinton','admin')
+    # def setUp(self):
+	#     '''
+    #     Function to create a user account before each test
+    #     '''
+    # self.new_user = User("Clinton","admin")
 
-    def test__init__(self):
+    def test__init(self):
         '''
 		Test to if check the initialization/creation of user instances is properly done
 		'''
-        self.assertEqual(self.new_user.first_name,'Clinton')
-		self.assertEqual(self.new_user.password,'admin')
+    self.assertEqual(self.new_user.username,'Clinton')
+    self.assertEqual(self.new_user.password,'admin')
 
-    #def test_save_user
+    def test_save_user(self):
+        '''
+		Test to check if the new users info is saved into the user list
+	    '''
+    self.new_user.register()
+    self.assertEqual(len(User.user_list),1)
+
+if __name__ == '__main__':
+	unittest.main()
+
