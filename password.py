181030ff4a4a92de7a514b478828337a6ba1c237
ClintonClin <clintonclin00@gmail.com>
2018-11-04 16:15:08 +0300

    added a function to verify a user

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 import random  
 from user import User, Info
-import pyperclip
+# import pyperclip
 
 def register_user(username, password):
     '''
@@ -13,9 +13,16 @@ def save_user(user):
     '''
     Created a user function to save the new user
     '''
-    User.save_user(user):
+    User.save_user(user)
 
-def create_info(username_name, account_name, info_details, password)
+def check_user(username, password):
+    '''
+    function for verifying  user
+    '''
+    checking_user = Info.check_User(username, password)
+    return checking_user
+
+def create_info(username_name, account_name, info_details, password):
     '''
     function to create new information of a user
     '''
@@ -37,18 +44,29 @@ def clipboard_info():
 def main():
     print("**"*30)
     print("Choose one option below \n 1. Create account \n 2. sign in \n 3. quit \n")
+    
     choice = input()
-    if input == "3":
-        break
+    if choice == "3":
+        sys.exit()
 
-    elif input == "1":
+    elif choice == "1":
         print("**"*30)
         print("Create a new account")
         print ("\n")
         username = input('Create a username')
         password= input("Create password")
         save_user(register_user(username, password))
-        print(f"Created account for {username} with password as {password}")
+        print("Account created successfully!!")
+
+    elif choice == "2":
+        print("**"*30)
+        print("Log in by entering your account credentials")
+        username = ("Enter your username")
+        password = ("Enter your password")
+
+
+
+
 
     
         
