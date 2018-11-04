f56e1d71c9be55b1bb06c78f22abd9635f551cfc
ClintonClin <clintonclin00@gmail.com>
2018-11-04 12:28:20 +0300

    created a user class for generating instatnces

--- user.py
+++ user.py
@@ -1,9 +1,11 @@
+import pyperclip
+import random
+
+global user_list
 class User:
     """
     Class that generates new instances of the user to sign up.
     """
+    info_list = [] #empty info list
     user_list = [] #Empty user list
-
-    def __init__(self,username,password):
-        self.username = username
-        self.password = password
+    
