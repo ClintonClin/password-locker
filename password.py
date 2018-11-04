7d53c7de7c68eedb4b5283c42882a459cf787973
ClintonClin <clintonclin00@gmail.com>
2018-11-04 12:42:25 +0300

    created a function for creating info of a user

--- password.py
+++ password.py
@@ -14,3 +14,11 @@ def save_user(user)
     Created a user function to save the new user
     '''
     User.save_user(user)
+
+def create_info(username_name, account_name, info_details, password)
+    '''
+    function to create new information of a user
+    '''
+    new_info = Info(username_name,account_name, info_details, password)
+    return new_info
+    
